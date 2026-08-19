3ded0c6643e0b167db63805f113445529a817f0a
faezaproject-gif <faezaproject@gmail.com>
2026-08-19 21:04:30 +0700

    Create app.js

---
+++ js/js/js/app.js
@@ -0,0 +1,24 @@
+/* =========================================================
+   FAEZAPROJECT V2
+   APPLICATION
+   ========================================================= */
+
+(function () {
+    "use strict";
+
+    document.addEventListener(
+        "DOMContentLoaded",
+        function () {
+
+            document.documentElement.classList.add(
+                "js-ready"
+            );
+
+            console.log(
+                "FaezaProject V2 siap."
+            );
+
+        }
+    );
+
+})();
